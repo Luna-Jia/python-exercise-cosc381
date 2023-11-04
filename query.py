be03b8c07b8553fad9be09255694881c55a1d828
Meng Luna Jia <mjia2@emich.edu>
2023-11-04 02:24:23 +0000

    feat: Implement "search movie by name" feature in menu
    This feature allows users to search movies with movie names.
    
    issue #5

--- query.py
+++ query.py
@@ -1,8 +1,9 @@
 from movies import Movies
 
-movies = Movies('./movies.txt')
+
 
 def main():
+    movies = Movies('./movies.txt')
     while True:
         print("q: quit")
         print("sn: search movie names")
@@ -13,6 +14,7 @@ def main():
         if(option == 'q'):break
         elif (option == "sn"):
             name = input("Enter a search term:")
+            movies.search_movie_names(name)
         elif (option == "sc"):
             cast = input("Enter a search term:")
         elif (option == "list"):
