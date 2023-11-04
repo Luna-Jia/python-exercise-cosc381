56c441f560ffbf86a77fafc702efce4863a62dee
Meng Luna Jia <mjia2@emich.edu>
2023-11-04 00:40:41 +0000

    feat: add a menu for searching movies

--- query.py
+++ query.py
@@ -1,3 +1,26 @@
 from movies import Movies
 
 movies = Movies('./movies.txt')
+
+def main():
+    while True:
+        print("q: quit")
+        print("sn: search movie names")
+        print("sc: search cast")
+        print("list: print all the movie names")
+        option = input('')
+
+        if(option == 'q'):break
+        elif (option == "sn"):
+            name = input("Enter a search term:")
+        elif (option == "sc"):
+            cast == input("Enter a search term:")
+        elif (option == "list"):
+            print ("hello")
+main()
+
+
+
+
+
+
